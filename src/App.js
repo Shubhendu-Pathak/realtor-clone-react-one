7bca9ed57b8b5cac858bb48aa400d27324406bbe
Shubhendu-Pathak <93846723+Shubhendu-Pathak@users.noreply.github.com>
2023-06-03 19:18:42 +0530

    create listing page and swiper to add image as slider

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from './pages/CreateListing'
 import EditListing from './pages/EditListing'
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -27,12 +28,17 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
           <Route path="create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
           </Route>
           <Route path="edit-listing" element={<PrivateRoute />}>
             <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
+
         </Routes>
       </Router>
       <ToastContainer
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import React,{useEffect, useState} from 'react'
+import { useParams } from 'react-router'
+import { db } from '../firebase'
+import Spinner from '../components/Spinner'
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "swiper/css/bundle";
 
 function Listing() {
+const params = useParams()
+const [listing,setListing] = useState(null)
+const [loading, setLoading] = useState(true);
+const [shareLinkCopied, setShareLinkCopied] = useState(false);
+const [contactLandlord, setContactLandlord] = useState(false);
+SwiperCore.use([Autoplay, Navigation, Pagination]);
+
+useEffect(()=>{
+async function fetchListing(){
+const docRef = doc(db,'listings',params.listingId)
+const docSnap = await getDoc(docRef)
+if(docSnap.exists()){
+setListing(docSnap.data())
+setLoading(false)
+}
+
+}
+fetchListing()
+
+},[params.listingId])
+
+if (loading) {
+  return <Spinner />;
+}
+
   return (
-    <div>Listing</div>
+   <main>
+    <Swiper
+     slidesPerView={1}
+     navigation
+     pagination={{ type: "progressbar" }}
+     effect="fade"
+     modules={[EffectFade]}
+     autoplay={{ delay: 3000 }}
+    >
+    {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+    </Swiper>
+   </main>
   )
 }
 
